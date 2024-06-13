5bc9281ad18c9e51c3a334a1446f8f8ce01175d6
root <root@YEAWE1.hhbrown.com>
2024-06-13 13:26:16 -0400

    adding solution

---
+++ ChatAppNET8/ChatAppNET8/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAppNET8.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            string processedMessage = ProcessMessage(user, message);
+
+            await Clients.All.SendAsync("ReceiveMessage", user, processedMessage);
+        }
+
+        private string ProcessMessage(string user, string message)
+        {
+            return $"{message.ToUpper()}";
+        }
+    }
+}
